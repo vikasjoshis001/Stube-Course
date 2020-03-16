27da6965b26f6f7dd20f4612ce6e8b9650d83cb0
vikasjoshis001 <vikasjoshis001@gmail.com>
2020-03-16 17:32:18 +0530

    Second step

--- Stube/src/app/course-detail/course-detail.component.ts
+++ Stube/src/app/course-detail/course-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,OnDestroy} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { getUrlScheme } from '@angular/compiler';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component
 ({
@@ -12,6 +13,15 @@ export class CourseDetailComponent implements OnInit,OnDestroy
 {
   private routeSub:any;
   link="string";
+  // C dict
+  C = ["Bjzfag1zZPg","__JKmT5EFb8","92HLfZHH814","4RvPihzs6uw","HXkuoR63_Ts","dw889ubtFR8","FZE_uBFkbqU","iKyYV-BbPCg","gIxYS1n00g0"];
+  // C dict
+
+  // Angular dict
+  Angular = ["9wc8Gv8X49U","8QOzAmWyouA","OuKO2dVU0U","XGEsZmoZNbw","E1lSYoLf5P4","uBeDt_poi00","1GnMB9ge6tg","N0ldJ9jITE8","6wD4V0rvlDI","nzyJ9imm29w",]
+  // Angular dict
+
+  // Normal dict
   course_list =
   [
     {
@@ -75,8 +85,10 @@ export class CourseDetailComponent implements OnInit,OnDestroy
 
     },
   ]
+  // Normal dict
 
-  constructor( private route:ActivatedRoute) 
+
+  constructor( private sanitizer:DomSanitizer , private route:ActivatedRoute) 
   { }
 
   ngOnInit(): void {
@@ -88,5 +100,20 @@ export class CourseDetailComponent implements OnInit,OnDestroy
   ngOnDestroy(): void {
     this.routeSub.unsubscribe()
   }
+
+  // Embeded Videos
+
+          // Angular 9.0
+  getangular(video)
+    {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/'+ video + '?start=5');  
+    }
+
+          // C programming
+  getC(video)
+    {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/'+ video + '?start=5');  
+    }
+        
 
 }
